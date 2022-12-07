2a99133c8e7a3953423de8d75a9d2271f2bce702
Adittya Hasan Adi <adihasanrifat@gmail.com>
2022-12-07 23:45:13 +0300

    connect-with-api

---
+++ plugin64/build/generated/source/buildConfig/debug/com/skullshooter/ssplugin64/app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.skullshooter.ssplugin64.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.skullshooter.ssplugin64.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.6";
+}
